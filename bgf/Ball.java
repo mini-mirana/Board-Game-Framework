e3b4447a04a8852159b564b91d5c338f1d2978cc
Mirana <Mini_Mirana@yahoo.com>
2019-01-24 19:56:11 +0330

    Implement moveInner

--- bgf/Ball.java
+++ bgf/Ball.java
@@ -94,7 +94,48 @@ class ManageBoard {
         }
     }
     public void moveInner(int x, int y,int i,int j){
+        switch(x) {
+            case 1:
+                HBox pieceTemp = Ball.lists.get(x).get(y);
+                String text = ((Label) (pieceTemp.getChildren().get(0))).getText();
+                pieceTemp.setVisible(false);
 
+                HBox listTemp = Ball.lists.get(i).get(j);
+                listTemp.getChildren().remove(0);
+                Label label = new Label(text);
+                listTemp.getChildren().add(label);
+                break;
+            case 2:
+                HBox pieceTemp2 = Ball.lists.get(x).get(y);
+                String text2 = ((Label) (pieceTemp2.getChildren().get(0))).getText();
+                pieceTemp2.setVisible(false);
+
+                HBox listTemp2 = Ball.lists.get(i).get(j);
+                listTemp2.getChildren().remove(0);
+                Label label2 = new Label(text2);
+                listTemp2.getChildren().add(label2);
+                break;
+            case 3:
+                HBox pieceTemp3 = Ball.lists.get(x).get(y);
+                String text3 = ((Label) (pieceTemp3.getChildren().get(0))).getText();
+                pieceTemp3.setVisible(false);
+
+                HBox listTemp3 = Ball.lists.get(i).get(j);
+                listTemp3.getChildren().remove(0);
+                Label label3 = new Label(text3);
+                listTemp3.getChildren().add(label3);
+                break;
+            case 4:
+                HBox pieceTemp4 = Ball.lists.get(x).get(y);
+                String text4 = ((Label) (pieceTemp4.getChildren().get(0))).getText();
+                pieceTemp4.setVisible(false);
+
+                HBox listTemp4 = Ball.lists.get(i).get(j);
+                listTemp4.getChildren().remove(0);
+                Label label4 = new Label(text4);
+                listTemp4.getChildren().add(label4);
+                break;
+        }
     }
     public void isMoveValidOutter(int cat, int y,int i,int j){
 
