f7522d039d4dc6ce523af1f83c9d9112e1da8535
ALi <s.arme@ya.ru>
2019-01-25 21:28:18 +0330

    Instantiate Manageboard java class in c++ and import move and shoMsg methods

--- bgf/master.h
+++ bgf/master.h
@@ -8,6 +8,10 @@ public:
 	static jni::Class Ball;
 	static jni::method_t exe;
 	static jni::Class ManageBoard;
+	static jni::Object mB;
+	static jni::method_t showMsg;
+	static jni::method_t movePiece;
+
 };
 
 volatile jni::Vm Master::vm = *new jni::Vm("C:\\Program Files (x86)\\Java\\jre-9\\bin\\server\\jvm.dll");
@@ -16,3 +20,6 @@ jni::Object Master::i = Integer.newInstance("1000");
 jni::Class Master::Ball = jni::Class("Ball");
 jni::method_t Master::exe = Ball.getStaticMethod("exe", "(IIIIIIILjava/lang/String;)I");
 jni::Class Master::ManageBoard = jni::Class("ManageBoard");
+jni::Object Master::mB = ManageBoard.newInstance();
+jni::method_t Master::movePiece = ManageBoard.getMethod("movePiece", "(IIII)V");
+jni::method_t Master::showMsg = Ball.getStaticMethod("showMsg", "(Ljava/lang/String;)V");
